cf1dcd5a652a1019d93bf5724e9daa441c9b1cf4
Sandro Rybárik <sandro.rybarik@gmail.com>
2016-08-21 09:37:18 +0000

    updated createOrUpdate methods

--- app/Http/Controllers/CostController.php
+++ app/Http/Controllers/CostController.php
@@ -24,47 +24,73 @@ class CostController extends Controller
 	* Cost handler, create or update
 	* (náklady)
 	*/
-    public function create (Request $request){
-    	$this->validate($request, [
-    		"item" => 'required',
-    		"cost" => 'required',
-    		"supplier" => 'required',
-    		"project_id" => "required"
-		]);
+  //   public function create (Request $request){
+  //   	$this->validate($request, [
+  //   		"item" => 'required',
+  //   		"cost" => 'required',
+  //   		"supplier" => 'required',
+  //   		"project_id" => "required"
+		// ]);
 
-    	//$cost = Cost::find($id);
+  //   	//$cost = Cost::find($id);
  
-		$newCost = new Cost;
-    	$newCost ->item = $request->item;
-    	$newCost ->cost = $request->cost;
-    	$newCost ->supplier = $request ->supplier;
-    	// hidden input!
-    	$newCost ->project_id = $request ->project_id;
-    	$newCost ->save();
+		// $newCost = new Cost;
+  //   	$newCost ->item = $request->item;
+  //   	$newCost ->cost = $request->cost;
+  //   	$newCost ->supplier = $request ->supplier;
+  //   	// hidden input!
+  //   	$newCost ->project_id = $request ->project_id;
+  //   	$newCost ->save();
 
-        return response()-json(['feedback' => 'created'], 201);
+  //       return response()-json(['feedback' => 'created'], 201);
 
-    }
+  //   }
 
-    public function update (Request $request, $id){
+  //   public function update (Request $request, $id){
 
-    	$this->validate($request, [
-    		"uitem" => 'required',
-    		"ucost" => 'required',
-    		"usupplier" => 'required'
-		]);
+  //   	$this->validate($request, [
+  //   		"uitem" => 'required',
+  //   		"ucost" => 'required',
+  //   		"usupplier" => 'required'
+		// ]);
 
-    	$cost = Cost::find($id);
+  //   	$cost = Cost::find($id);
 
-    	if($cost == null){
-    		return back();
-    	}
+  //   	if($cost == null){
+  //   		return back();
+  //   	}
 
-    	$cost ->item = $request->uitem;
-    	$cost ->cost = $request->ucost;
-    	$cost ->supplier = $request ->usupplier;
-    	$cost ->save();
+  //   	$cost ->item = $request->uitem;
+  //   	$cost ->cost = $request->ucost;
+  //   	$cost ->supplier = $request ->usupplier;
+  //   	$cost ->save();
 
-        return response()->json(['feedback' => 'updated'], 201);
+  //       return response()->json(['feedback' => 'updated'], 201);
+  //   }
+
+    public function createOrUpdate (Request $request){
+
+      $this->validate($request,[
+        "rows.*.id" => 'required',
+        "rows.*.item" => 'required',
+        "rows.*.supplier" => 'required',
+        "rows.*.cost" => 'required',
+        "rows.*.project_id" => 'required'
+      ]);
+
+        $count = count($request->rows());
+        $feedback = 0;
+      foreach ($request->rows as $row) {
+        $cost = Cost::where('id', $row->id)->first();
+        if($cost !== null){
+            //update
+            $cost->update($row);
+        }else{
+            //create
+            Cost::create($row);
+            $feedback++;
+        }
+      }
+        return response()->json(['feedback' => "$count / $feedback succesfuly created or updated"]);
     }
 }
--- app/Http/Controllers/RevenueController.php
+++ app/Http/Controllers/RevenueController.php
@@ -18,55 +18,81 @@ class RevenueController extends Controller
     {
         //$this->middleware('auth');
     }
-    
+
   	/**
   	* Revenue handler, create or update
   	* (primy)
   	*/
 
-    public function create(Request $request){
-    	$this->validate($request, [
-    		"item" => 'required',
-    		"cost" => 'required',
-    		"quantity" => 'required',
-    		"project_id" => "required"
-		]);
+  //   public function create(Request $request){
+  //   	$this->validate($request, [
+  //   		"item" => 'required',
+  //   		"cost" => 'required',
+  //   		"quantity" => 'required',
+  //   		"project_id" => "required"
+		// ]);
 
-    	// if entity does not exists, create it
-		$newRevenue = new Revenue;
-    	$newRevenue ->item = $request->item;
-    	$newRevenue ->cost = $request->cost;
-    	$newRevenue ->quantity = $request ->quantity;
-    	// hidden input!
-    	$newRevenue ->project_id = $request ->project_id;
-    	$newRevenue ->save();
+  //   	// if entity does not exists, create it
+		// $newRevenue = new Revenue;
+  //   	$newRevenue ->item = $request->item;
+  //   	$newRevenue ->cost = $request->cost;
+  //   	$newRevenue ->quantity = $request ->quantity;
+  //   	// hidden input!
+  //   	$newRevenue ->project_id = $request ->project_id;
+  //   	$newRevenue ->save();
 
-      //\Session::put('revenue_creation_status', 'Položka úspešne vložená.');
+  //     //\Session::put('revenue_creation_status', 'Položka úspešne vložená.');
 
-      return response()->json(['feedback' => 'created'], 201);
+  //     return response()->json(['feedback' => 'created'], 201);
+  //   }
+
+  //  	public function update (Request $request, $id){
+  //  		//validate request
+  //   	$this->validate($request, [
+  //   		"uitem" => 'required',
+  //   		"ucost" => 'required',
+  //   		"uquantity" => 'required',
+		// ]);
+
+  //  		$revenue = Revenue::find($id);
+
+  //  		if($revenue == null){
+  //  			return back();
+  //  		}
+
+  //   	$revenue ->item = $request->uitem;
+  //   	$revenue ->cost = $request->ucost;
+  //   	$revenue ->quantity = $request ->uquantity;
+  //   	$revenue ->save();
+
+  //     //\Session::put('revenue_update_status', 'Položka úspešne zmenená.');
+
+  //     return response()->json(['feedback' => 'created'], 201);
+  //  	}
+
+    public function createOrUpdate (Request $request){
+
+      $this->validate($request,[
+        "rows.*.id" => 'required',
+        "rows.*.item" => 'required',
+        "rows.*.quantity" => 'required',
+        "rows.*.cost" => 'required',
+        "rows.*.project_id" => 'required'
+        ]);
+
+      $count = count($request->rows());
+      $feedback = 0;
+      foreach ($request->rows as $row) {
+        $revenue = Revenue::where('id', $row->id)->first();
+        if($revenue !== null){
+            //update
+          $revenue->update($row);
+        }else{
+            //create
+          Revenue::create($row);
+          $feedback++;
+        }
+      }
+      return response()->json(['feedback' => "$count / $feedback succesfuly created or updated"]);
     }
-
-   	public function update (Request $request, $id){
-   		//validate request
-    	$this->validate($request, [
-    		"uitem" => 'required',
-    		"ucost" => 'required',
-    		"uquantity" => 'required',
-		]);
-
-   		$revenue = Revenue::find($id);
-
-   		if($revenue == null){
-   			return back();
-   		}
-
-    	$revenue ->item = $request->uitem;
-    	$revenue ->cost = $request->ucost;
-    	$revenue ->quantity = $request ->uquantity;
-    	$revenue ->save();
-
-      //\Session::put('revenue_update_status', 'Položka úspešne zmenená.');
-
-      return response()->json(['feedback' => 'created'], 201);
-   	}
-}
+  }
